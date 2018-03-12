4b9add7748688938318e7d354d2b90519879ef04
INTEXSOFT\yauheni.hanchar <yauheni.hanchar@intexsoft.by>
2018-03-12 12:03:00 +0300

    config

--- src/main/java/com/config/AppConfig.java
+++ src/main/java/com/config/AppConfig.java
@@ -1,18 +1,19 @@
 package com.config;
 
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.*;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Configuration searches for the service and loads app.properties
  */
 @Configuration
-@ComponentScan("com.service")
-@PropertySource("app.properties")
 @EnableScheduling
+@ComponentScan("com.service")
+@PropertySources({
+        @PropertySource(value = "classpath:app.properties", ignoreResourceNotFound = true),
+        @PropertySource(value = "file:./app.properties", ignoreResourceNotFound = true)
+}
+)
 public class AppConfig {
 
 }
